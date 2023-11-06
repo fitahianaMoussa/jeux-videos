53637eb5c4c63bfd0f837059d674d98dd0cb0015
fitahiana <fitahianamoussa3@gmail.com>
2023-11-06 12:58:25 +0100

    Build platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,24 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenre";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null >(null);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
   return (
     <Box>
       <NavBar />
       <Grid templateColumns="repeat(12, 1fr)" gap={4} p={4}>
         <GridItem colSpan={1}>
-          <GenreList onSelectedGenre={(genre)=> setSelectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectedGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
         <Spacer />
         <GridItem colSpan={10}>
+          <PlatformSelector/>
           <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,28 +1,43 @@
-import { Spinner ,HStack, Image, List, ListItem, Button } from "@chakra-ui/react";
+import {
+  Spinner,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Button,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenre";
 
 interface Props {
-  onSelectedGenre : (genre: Genre) => void
+  onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({onSelectedGenre}: Props) => {
-  const { data ,isLoading, error} = useGenres();
+const GenreList = ({ selectedGenre, onSelectedGenre }: Props) => {
+  const { data, isLoading, error } = useGenres();
   if (isLoading) {
-    return <Spinner/>
+    return <Spinner />;
   }
   if (error) {
-    return null 
+    return null;
   }
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px' >
+        <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
               boxSize="32px"
               borderRadius={8}
               src={genre.image_background}
             />
-            <Button fontSize="lg" variant="link" onClick={()=> onSelectedGenre(genre)} > {genre.name} </Button>
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              fontSize="lg"
+              variant="link"
+              onClick={() => onSelectedGenre(genre)}>
+              {" "}
+              {genre.name}{" "}
+            </Button>
           </HStack>
         </ListItem>
       ))}
